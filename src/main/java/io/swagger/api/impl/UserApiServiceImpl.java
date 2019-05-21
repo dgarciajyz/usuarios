4b58cba1025056db27456ddcea25372a4caad878
ManuJazz <mjesusaz@alumnos.unex.es>
2019-05-21 11:29:07 +0000

    Implementacion de la API

--- src/main/java/io/swagger/api/impl/UserApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/UserApiServiceImpl.java
@@ -5,8 +5,8 @@ import io.swagger.model.*;
 
 import io.swagger.model.Usuario;
 
-import java.util.Map;
-import java.util.List;
+//import java.util.Ma/p;
+import java.util.*;
 import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
@@ -17,19 +17,27 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2019-05-21T11:07:10.746Z[GMT]")public class UserApiServiceImpl extends UserApiService {
+    List<Usuario> users = new LinkedList<Usuario>();
     @Override
     public Response inicioSesionUsuarios( @NotNull String username,  @NotNull String password, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        String mess="No puedes iniciar sesion";
+        for(int i = 0; i<users.size();i++){
+            if(users.get(i).getUsername().equals(username)&&users.get(i).getPassword().equals(password))
+                mess="Sesion iniciada";
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, mess)).build();
     }
     @Override
     public Response logoutUsuario(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Sesion Cerrada!")).build();
     }
     @Override
     public Response registroUsuarios(Usuario body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        Usuario u = new Usuario();
+        u.setIdUsuario(body.getIdUsuario());
+        u.setUsername(body.getUsername());
+        u.setPassword(body.getPassword());
+        users.add(u);
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Registro Correcto")).build();
     }
 }
